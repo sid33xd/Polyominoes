1788e86474d4b780f6ad9c9e5d19686a5c28db70
sid33xd <168820689+sid33xd@users.noreply.github.com>
2026-04-27 13:56:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/whisker-fit/' : '/',
+  base: command === 'build' ? '/Polyominoes/' : '/',
   build: {
     outDir: 'dist',
   },
